5c93e9c186130380560131182f3526781a2d6646
David Armstrong Lewis <david@armstronglewis.com>
2021-05-24 10:31:10 -0700

    Remove rip.nix

--- system/darwin/configuration.nix
+++ system/darwin/configuration.nix
@@ -9,7 +9,7 @@ let
   theme = import (layer "theme.nix") {
     inherit pkgs;
     theme      = "gruvbox";
-    variant    = "dark";
+    variant    = "light";
     fontFamily = "DM Mono";
     fontSize   = 13.5;
   };
